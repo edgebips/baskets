45314c7c47a888eeff9c57caa5218b00cb3f0f58
Martin Blais <blais@furius.ca>
2020-07-17 09:21:53 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 """Install script for Ameritrade package.
 """
-__copyright__ = "Copyright (C) 2018  Martin Blais"
+__copyright__ = "Copyright (C) 2018-2020  Martin Blais"
 __license__ = "GNU GPLv2"
 
 import sys
+
+from setuptools import setup
 
 if sys.version_info[:2] < (3,):
     raise SystemExit("ERROR: Insufficient Python version; you need v3 or higher.")
 
 setup(
-    name="ameritrade",
-    version='1.0b1',
-    description="Python bindings to the Ameritrade API",
+    name="baskets",
+    version='0.1',
+    description="ETF Holdings Downloader, Parser and Disaggregator",
 
     license="GNU GPLv2 only",
     author="Martin Blais",
     author_email="blais@furius.ca",
-    url="http://github.com/blais/ameritrade",
-    download_url="http://github.com/blais/ameritrade",
+    url="http://github.com/blais/baskets",
+    download_url="http://github.com/blais/baskets",
 
-    packages = ['ameritrade'],
+    packages = [
+        'baskets',
+        'baskets/issuers',
+    ],
 
     install_requires = [
         'requests',
